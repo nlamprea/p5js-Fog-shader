041aca5aeb601885ba6d58c60b8f69cfb05089e2
nlamprea <nlamprea@unal.edu.co>
2024-05-31 23:32:57 -0500

    a

--- src/sketch.js
+++ src/sketch.js
@@ -1,36 +1,61 @@
-let angle =0;
-let image;
-let slider;
+let angle = 0;
+let img;
+let nearSlider, farSlider;
 
-function preload(){
- image = loadImage("https://webglfundamentals.org/webgl/resources/f-texture.png")
+function preload() {
+  img = loadImage("https://webglfundamentals.org/webgl/resources/f-texture.png");
 }
 
 function setup() {
-  createCanvas(400, 300, WEBGL);
-  slider = createSlider(10,100,47)
-  slider.position(10, height + 10);
+  createCanvas(300, 200, WEBGL);
+
+  nearSlider = createSlider(0, 255, 150);
+  nearSlider.position(10, height + 10);
+  let nearLabel = createP('FogNear');
+  nearLabel.position(nearSlider.x * 2 + nearSlider.width, height + 5);
+
+  farSlider = createSlider(0, 255, 50);
+  farSlider.position(10, height + 40);
+  let farLabel = createP('FogFar');
+  farLabel.position(farSlider.x * 2 + farSlider.width, height + 35);
 }
 
 function draw() {
   background(200);
-  
   rectMode(CENTER);
-  //fill(0,0,150);
 
-  let val = slider.value();
-  ambientLight(val, val, val);
+  let nearVal = nearSlider.value();
+  let farVal = farSlider.value();
 
-  //directionalLight(250, 250, 250, 0, -1, 0);
+  if (nearVal === 0 && farVal === 0) {
+    return;
+  }
 
-  rotateX(angle)
-  rotateY(angle)
-  rotateZ(angle)
+  let numBoxes = 20; 
+  let initialSpacing = 500; 
 
-  noStroke();
+  for (let i = 0; i < numBoxes; i++) {
+    push();
+    let spacing = initialSpacing * pow(0.9, i);
+    let offset = map(i, 0, numBoxes - 1, -300, 300);
+    translate(offset, 0, -spacing * i);
 
-  texture(image)
-  box(100)
+    let brightness = map(i, 0, numBoxes - 1, nearVal, farVal);
+    let opacity = map(i, 0, numBoxes - 1, 255, 50);
 
-  angle += 0.02;  
+    tint(brightness, brightness, brightness, opacity);
+
+    let boxSize = map(i, 0, numBoxes - 1, 100, 20);
+
+    rotateX(angle);
+    rotateY(angle);
+    rotateZ(angle);
+
+    noStroke();
+    texture(img);
+    box(boxSize);
+    pop();
+  }
+
+  angle += 0.02;
 }
