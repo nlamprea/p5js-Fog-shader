5fc569306773de38275a4429e70735d574a981ac
nlamprea <nlamprea@unal.edu.co>
2024-06-03 21:55:22 -0500

    e

--- src/sketch.js
+++ src/sketch.js
@@ -11,12 +11,12 @@ function preload() {
 function setup() {
   createCanvas(300, 200, WEBGL);
 
-  nearSlider = createSlider(0.3, 40, 13.0);
+  nearSlider = createSlider(4, 50, 13.5);
   nearSlider.position(10, height + 10);
   let nearLabel = createP('FogFar');
   nearLabel.position(nearSlider.x * 2 + nearSlider.width, height + 5);
 
-  farSlider = createSlider(-40, 0.3, -13.0);
+  farSlider = createSlider(-25, 0, -13.0);
   farSlider.position(10, height + 40);
   let farLabel = createP('FogNear');
   farLabel.position(farSlider.x * 2 + farSlider.width, height + 35);
@@ -30,24 +30,24 @@ function draw() {
   let farVal = farSlider.value();
 
 
-  let numBoxes = 10; 
+  let numBoxes = 20; 
   let initialSpacing = 300; 
 
   shader(fogShader);
-  fogShader.setUniform('u_fogColor', [0.8, 0.9, 1, 1]);
+  
   for (let i = 0; i < numBoxes; i++) {
     push();
 
     fogShader.setUniform('uTexture', img);
     
-    
-    fogShader.setUniform('uFogNear', map(i, 0, numBoxes, 0, nearVal));
-    fogShader.setUniform('uFogFar', map(i, 0, numBoxes, 0, farVal));
-    let spacing = initialSpacing * pow(0.9, i);
+    fogShader.setUniform('u_fogColor', [0.8, 0.9, 1, 1]);
+    fogShader.setUniform('uFogNear', map(i, 0, numBoxes, 0.3, nearVal));
+    fogShader.setUniform('uFogFar', map(i, 0, numBoxes, farVal, 0.3));
+    let spacing = initialSpacing * pow(0.96, i);
     let offset = map(i, 0, numBoxes - 1, -300, 300);
     translate(offset, 0, -spacing * i);
 
-    let boxSize = map(i, 0, numBoxes - 1, 100, 20);
+    let boxSize = map(i, 0, numBoxes, 100, 20);
     
 
     rotateX(angle);
